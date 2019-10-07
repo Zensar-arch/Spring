3fde63872f5fe753757797358e6ea753bccebad7
Zensar-arch <55914257+Zensar-arch@users.noreply.github.com>
2019-10-07 18:26:38 +0530

    Add files via upload

---
+++ SetterInjection/src/main/java/com/zensar/spring/main/ZenserIdolMain.java
@@ -0,0 +1,15 @@
+package com.zensar.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ZenserIdolMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext p=new ClassPathXmlApplicationContext("performers.xml");
+	
+
+	}
+
+}
