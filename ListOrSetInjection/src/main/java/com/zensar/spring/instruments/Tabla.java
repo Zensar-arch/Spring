35233a4c900314b91bb599133bd3ff693adf3dc3
Zensar-arch <55914257+Zensar-arch@users.noreply.github.com>
2019-10-09 19:47:24 +0530

    Add files via upload

---
+++ ListOrSetInjection/src/main/java/com/zensar/spring/instruments/Tabla.java
@@ -0,0 +1,15 @@
+package com.zensar.spring.instruments;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Tabla implements Instrument {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("tab tab tab");
+
+	}
+
+}
